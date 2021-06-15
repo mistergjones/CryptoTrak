bcf967f48dcf864540c06f2ae68950b833af590d
Glen <mistergjones@yahoo.com.au>
2021-06-15 11:29:34 +1000

    More changes to advanced charting and JS code

--- js/cryptotrak.js
+++ js/cryptotrak.js
@@ -14,7 +14,7 @@ async function getBitcoin() {
         );
 
         // draw the actual chart
-        bitcoinChart(dataLabels, dataPoints);
+        bitcoinChart(dataLabels, dataPoints, "myChart1");
 
         // now plot the actual chart
     } catch (error) {
@@ -27,7 +27,7 @@ async function getBollingerEthereum() {
     //
     try {
         const historicalEthereumData = await axios.get(
-            "https://api.coincap.io/v2/assets/ethereum/history?interval=d1&start=1577797200000&end=1580475600000"
+            "https://api.coincap.io/v2/assets/ethereum/history?interval=d1&start=1618247554000&end=1623517954000"
         );
 
         // extract the data out in a format data array format chart.js requires. Destructure the returned dataObject
@@ -41,19 +41,20 @@ async function getBollingerEthereum() {
         );
 
         // with the dataPoints, need to calculate a rolling 5 day average
-
         //var movingAverageDataPoints = calculateMovingAverage(dataPoints);
 
         // var movingAverageDataPoints = ma(dataPoints);
-        var { theMeanOf5Days, upper_bollinger_band, lower_bollinger_band } = ma(
-            dataPoints
-        );
+        var {
+            theMeanOfEach5DayBlock,
+            upper_bollinger_band,
+            lower_bollinger_band,
+        } = calculateMovingAverage(dataPoints);
 
         // draw the actual chart
         ethereumChart(
             dataLabels,
             dataPoints,
-            theMeanOf5Days,
+            theMeanOfEach5DayBlock,
             upper_bollinger_band,
             lower_bollinger_band
         );
@@ -83,7 +84,7 @@ function massageData(cryptoDataObject) {
     };
 }
 
-function bitcoinChart(dataLabels, dataPoints) {
+function bitcoinChart(dataLabels, dataPoints, myChart) {
     // chartjs seesm to ahve the data in an Object Format
     const data = {
         labels: dataLabels,
@@ -106,7 +107,8 @@ function bitcoinChart(dataLabels, dataPoints) {
     };
 
     // put the chart in the CSS ID element
-    var myChart = new Chart(document.getElementById("myChart1"), config);
+    // var myChart = new Chart(document.getElementById("myChart1"), config);
+    var myChart = new Chart(document.getElementById(myChart), config);
 }
 
 function ethereumChart(
@@ -169,6 +171,8 @@ function ethereumChart(
             pointRadius: 0,
             tension: 0.5,
             lineThickness: 5,
+            responsive: true,
+            maintainAspectRatio: true,
         },
     };
 
@@ -177,93 +181,64 @@ function ethereumChart(
 }
 
 function calculateMovingAverage(dataPoints) {
-    const tempArray = [10, 13, 12, 11, 10, 14, 7, 11, 12, 9, 11, 8, 10];
-
-    // conver the string elements of the dataPoints to floats
+    // convert the string elements of the dataPoints to floats
     dataPoints = dataPoints.map(Number);
 
-    var movingAverage = 5;
-    var arrOf5DayAverages = [];
+    // declare variables
+    var numberOfMovingAverageDays = 5;
+    var theMeanOfEach5DayBlock = [];
 
-    var i = 0;
+    var loopIterator = 0;
+    // 1.0 calcule the mean of each 5 day block and append it to an array
     // while condition determines how many blocks of days in the array to calculate from
-    while (i <= dataPoints.length - movingAverage) {
-        // console.log(dataPoints[i]);
+    while (loopIterator <= dataPoints.length - numberOfMovingAverageDays) {
+        // reset the running total to zero after each j loop
         var running_total = 0;
-        for (j = i; j < 5 + i; j++) {
+        for (j = loopIterator; j < 5 + loopIterator; j++) {
             running_total = running_total + dataPoints[j];
         }
-        console.log(i, running_total, running_total / movingAverage);
-
-        arrOf5DayAverages.push(running_total / movingAverage);
-        var running_total = 0;
-        i++;
+        // push the calculated moving average into the array
+        theMeanOfEach5DayBlock.push(running_total / numberOfMovingAverageDays);
+        // var running_total = 0;
+        loopIterator++;
     }
-    console.log("ASDADSFA");
-    console.log(arrOf5DayAverages);
-    return arrOf5DayAverages;
-}
-
-function ma(dataPoints2) {
-    const tempArray = [10, 13, 12, 11, 10, 14, 7, 11, 12, 9, 11, 8, 10];
-
-    // conver the string elements of the dataPoints to floats
-    dataPoints2 = dataPoints2.map(Number);
-
-    var numberOfMovingAverageDays = 5;
-    var theMeanOf5Days = [];
-
-    var i = 0;
-    // 1.0 calcule the mean of each 5 day block
-    // while condition determines how many blocks of days in the array to calculate from
-    while (i <= dataPoints2.length - numberOfMovingAverageDays) {
-        // console.log(dataPoints2[i]);
-        var running_total = 0;
-        for (j = i; j < 5 + i; j++) {
-            running_total = running_total + dataPoints2[j];
-        }
-        //console.log(i, running_total, running_total / numberOfMovingAverageDays);
-
-        theMeanOf5Days.push(running_total / numberOfMovingAverageDays);
-        var running_total = 0;
-        i++;
-    }
-    //console.log("ASDADSFA");
-    console.log(theMeanOf5Days);
 
     // 2.0 Subtract the mean from each block's numbers and square the result
+    // Calculate the Standard Deviation
+    var dataObjects = calculateStandardDeviation(
+        theMeanOfEach5DayBlock,
+        dataPoints,
+        numberOfMovingAverageDays
+    );
 
+    return dataObjects;
+}
+
+function calculateStandardDeviation(
+    theMeanOfEach5DayBlock,
+    dataPoints,
+    numberOfMovingAverageDays
+) {
     var gj = [];
     i = 0;
     var bollinger_band = [];
 
-    while (i < theMeanOf5Days.length) {
-        // console.log(dataPoints2[i]);
+    while (i < theMeanOfEach5DayBlock.length) {
+        // console.log(dataPoints[i]);
         var difference = 0;
         var running_squared_total = 0;
         for (j = i; j < 5 + i; j++) {
             // calculate the difference from the mean in each block
-            difference = dataPoints2[j] - theMeanOf5Days[i];
+            difference = dataPoints[j] - theMeanOfEach5DayBlock[i];
             // square the difference
             var gj = difference ** 2;
             // aggregate the total of squard difference
             running_squared_total = running_squared_total + gj;
-            // console.log(running_squared_total);
-            // console.log(
-            //     j,
-            //     dataPoints2[j],
-            //     theMeanOf5Days[i],
-            //     difference,
-            //     difference ** 2,
-            //     running_squared_total,
-            //     running_squared_total / numberOfMovingAverageDays,
-            //     Math.sqrt(running_squared_total / numberOfMovingAverageDays)
-            // );
         }
-        console.log(
-            `The SD on each group of 5 is: `,
-            Math.sqrt(running_squared_total / numberOfMovingAverageDays)
-        );
+        // console.log(
+        //     `The SD on each group of 5 is: `,
+        //     Math.sqrt(running_squared_total / numberOfMovingAverageDays)
+        // );
         // now need to return 2 * SDs for the Upper and Lower Bollinger Bands
         bollinger_band.push(
             Math.sqrt(running_squared_total / numberOfMovingAverageDays) * 2
@@ -271,37 +246,317 @@ function ma(dataPoints2) {
         i++;
     }
 
-    // insert the number of zeroes at teh start to cater for the moving average days. You need to have zeros
-    for (i = 1; i < numberOfMovingAverageDays; i++) {
-        bollinger_band.unshift(null);
-        theMeanOf5Days.unshift(null);
-    }
+    // insert the number of nulls at the start to cater for the moving average days. You need to have NULLS
+    bollinger_band = insertMovingAverageNulls(
+        bollinger_band,
+        numberOfMovingAverageDays
+    );
+    theMeanOfEach5DayBlock = insertMovingAverageNulls(
+        theMeanOfEach5DayBlock,
+        numberOfMovingAverageDays
+    );
 
-    // now need to add the bollinger band SD to above and below the Moving Average
     var upper_bollinger_band = [];
     var lower_bollinger_band = [];
-    for (i = numberOfMovingAverageDays - 1; i < theMeanOf5Days.length; i++) {
-        upper_bollinger_band.push(theMeanOf5Days[i] + bollinger_band[i]);
-        lower_bollinger_band.push(theMeanOf5Days[i] - bollinger_band[i]);
-        // console.log(
-        //     theMeanOf5Days[i],
-        //     bollinger_band[i],
-        //     theMeanOf5Days[i] + bollinger_band[i],
-        //     theMeanOf5Days[i] - bollinger_band[i]
-        // );
+    // now need to claculate the upper and lower bollinger bands based on "Bollinger band Standard Deviation" on the Moving Average
+    // from the end most element in the first 5 day block....
+    for (
+        var loopIterator = numberOfMovingAverageDays - 1;
+        loopIterator < theMeanOfEach5DayBlock.length;
+        loopIterator++
+    ) {
+        upper_bollinger_band.push(
+            theMeanOfEach5DayBlock[loopIterator] + bollinger_band[loopIterator]
+        );
+        lower_bollinger_band.push(
+            theMeanOfEach5DayBlock[loopIterator] - bollinger_band[loopIterator]
+        );
     }
 
-    // insert the number of zeroes at teh start to cater for the moving average days. You need to have zeros
-    for (i = 1; i < numberOfMovingAverageDays; i++) {
-        upper_bollinger_band.unshift(null);
-        lower_bollinger_band.unshift(null);
-    }
-
-    console.log(upper_bollinger_band, lower_bollinger_band);
+    // insert the number of nulls at the start to cater for the moving average days. You need to have NULLS
+    upper_bollinger_band = insertMovingAverageNulls(
+        upper_bollinger_band,
+        numberOfMovingAverageDays
+    );
+    lower_bollinger_band = insertMovingAverageNulls(
+        lower_bollinger_band,
+        numberOfMovingAverageDays
+    );
 
     return {
-        theMeanOf5Days: theMeanOf5Days,
+        theMeanOfEach5DayBlock: theMeanOfEach5DayBlock,
         upper_bollinger_band: upper_bollinger_band,
         lower_bollinger_band: lower_bollinger_band,
     };
+}
+
+// this function inserts nulls at the start of the array to cater for the 5 day moving average
+// it is also done to ensure chartjs DOES NOT plot NULLs
+function insertMovingAverageNulls(dataPoints, numberOfMovingAverageDays) {
+    for (i = 1; i < numberOfMovingAverageDays; i++) {
+        dataPoints.unshift(null);
+    }
+
+    return dataPoints;
+}
+
+/* Calculate the RSI for Bitcoin */
+async function getBitcoinRSI() {
+    try {
+        const historicalBitcoinData = await axios.get(
+            "https://api.coincap.io/v2/assets/bitcoin/history?interval=d1&start=1577797200000&end=1580475600000"
+        );
+
+        // extract the data out in a format data array format chart.js requires. Destructure the returned dataObject
+        var { dataPoints, dataLabels } = massageData(
+            historicalBitcoinData.data.data
+        );
+
+        // now do the RSI component
+        var rsiData = RSI();
+
+        // draw the actual chart. Also need to supply the RSI data
+        // bitcoinChart(dataLabels, dataPoints, "myChart3");
+        bitcoinChart(dataLabels, rsiData, "myChart3");
+
+        // now plot the actual chart
+    } catch (error) {
+        console.log("The errors is: ", error);
+    }
+}
+
+function RSI() {
+    var tempData = [
+        44.3389,
+        44.0902,
+        44.1497,
+        43.6124,
+        44.3278,
+        44.8264,
+        45.0955,
+        45.4245,
+        45.8433,
+        46.0826,
+        45.8931,
+        46.0328,
+        45.614,
+        46.282,
+        46.282,
+        46.0028,
+        46.0328,
+        46.4116,
+        46.2222,
+        45.6439,
+        46.2122,
+        46.2521,
+        45.7137,
+        46.4515,
+        45.7835,
+        45.3548,
+        44.0288,
+        44.1783,
+        44.2181,
+        44.5672,
+        43.4205,
+        42.6628,
+        43.1314,
+    ];
+
+    var movingAverageDays = 14;
+    // calculate the allDailyChanges on each day.
+
+    var allDailyChanges = [];
+    // the below is required to establish 0 on the first day
+    allDailyChanges.push(0);
+
+    // Start from position 1 as as we
+    for (var i = 1; i < tempData.length; i++) {
+        // console.log(tempData[i] - tempData[i - 1]);
+        allDailyChanges.push(tempData[i] - tempData[i - 1]);
+    }
+    // console.log("The chage array is:", allDailyChanges);
+
+    // calculate the allGains & allLosses for each day
+    var allGains = [];
+    var allLosses = [];
+    // allGains FIRST
+    for (i = 0; i < tempData.length; i++) {
+        if (allDailyChanges[i] > 0) {
+            allGains.push(allDailyChanges[i]);
+        } else {
+            allGains.push(0);
+        }
+    }
+    // allLosses SECOND
+    for (i = 0; i < tempData.length; i++) {
+        if (allDailyChanges[i] < 0) {
+            allLosses.push(allDailyChanges[i]);
+        } else {
+            allLosses.push(0);
+        }
+    }
+
+    // console.log("allGains are: ", allGains);
+    // console.log("allLosses are: ", allLosses);
+
+    // aggregate the only first 14 day aggregate of allGains & allLosses
+    var runningTotal = 0;
+
+    var eachDaysAverageGain = [];
+    var eachDaysAverageLoss = [];
+
+    for (i = 0; i < movingAverageDays; i++) {
+        runningTotal = runningTotal + allGains[i];
+    }
+    eachDaysAverageGain.push(runningTotal / movingAverageDays);
+    // console.log(eachDaysAverageGain);
+    runningTotal = 0;
+
+    for (i = 0; i < movingAverageDays; i++) {
+        runningTotal = runningTotal + allLosses[i];
+    }
+    eachDaysAverageLoss.push(runningTotal / movingAverageDays);
+    // console.log(eachDaysAverageLoss);
+    runningTotal = 0;
+
+    // now to calculate the 15th position and onwards RSI....
+
+    // 1. Take the previous days AVG gain (the 14th position AVG Gain) and multiply that
+    console.log(
+        (eachDaysAverageGain[0] * 13 + allGains[15]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[0] * 13 + allGains[15]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[1] * 13 + allGains[16]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[2] * 13 + allGains[17]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[3] * 13 + allGains[18]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[4] * 13 + allGains[19]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[5] * 13 + allGains[20]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[6] * 13 + allGains[21]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[7] * 13 + allGains[22]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[8] * 13 + allGains[23]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[9] * 13 + allGains[24]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[10] * 13 + allGains[25]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[11] * 13 + allGains[26]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[12] * 13 + allGains[27]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[13] * 13 + allGains[28]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[14] * 13 + allGains[29]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[15] * 13 + allGains[30]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[16] * 13 + allGains[31]) / movingAverageDays
+    );
+    eachDaysAverageGain.push(
+        (eachDaysAverageGain[17] * 13 + allGains[32]) / movingAverageDays
+    );
+
+    // 1. Take the previous days AVG Loss (the 14th position AVG Gain) and multiply that
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[0] * 13 + allLosses[15]) / movingAverageDays
+    );
+
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[1] * 13 + allLosses[16]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[2] * 13 + allLosses[17]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[3] * 13 + allLosses[18]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[4] * 13 + allLosses[19]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[5] * 13 + allLosses[20]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[6] * 13 + allLosses[21]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[7] * 13 + allLosses[22]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[8] * 13 + allLosses[23]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[9] * 13 + allLosses[24]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[10] * 13 + allLosses[25]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[11] * 13 + allLosses[26]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[12] * 13 + allLosses[27]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[13] * 13 + allLosses[28]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[14] * 13 + allLosses[29]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[15] * 13 + allLosses[30]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[16] * 13 + allLosses[31]) / movingAverageDays
+    );
+    eachDaysAverageLoss.push(
+        (eachDaysAverageLoss[17] * 13 + allLosses[32]) / movingAverageDays
+    );
+
+    console.log(eachDaysAverageGain, eachDaysAverageLoss);
+
+    // now calculate the RS factors and ensure POSITIVE NUMBERS
+    var rs = [];
+    for (i = 0; i < eachDaysAverageGain.length; i++) {
+        rs.push((eachDaysAverageGain[i] / eachDaysAverageLoss[i]) * -1);
+    }
+    console.log(rs);
+
+    var rsi = [];
+    // now calculate the RSI
+    for (i = 0; i < eachDaysAverageLoss.length; i++) {
+        if (eachDaysAverageLoss[i] == 0) {
+            rsi.push(100);
+        } else {
+            rsi.push(100 - 100 / (1 + rs[i]));
+        }
+    }
+
+    console.log(rsi);
+
+    return rsi;
 }
